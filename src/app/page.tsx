88ea4984f6d504f9031b8ed02adcc1aa2bcf745a
Your Name <you@example.com>
2023-07-26 01:00:51 +0300

    something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <Image
           className={styles.logo}
           src="/next.svg"
-          alt="Next.js Logo"
+          alt="Next.js  Logo"
           width={180}
           height={37}
           priority
